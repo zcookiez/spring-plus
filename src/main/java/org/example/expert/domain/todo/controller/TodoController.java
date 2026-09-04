5132cc887ecd08cddfe88586941309c792f04e46
zcookiez <annie9434@gmail.com>
2026-09-04 10:29:43 +0900

    style: 사용하지 않는 import문 정리

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
